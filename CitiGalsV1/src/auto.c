208638d54e827b45f0a832069d1f2b80e53e7e2e
EmilyH7 <georgiannahannon@gmail.com>
2017-11-04 14:26:52 -0700

    added autonomous example and chassis set function
    still have to work through bugs on opcontrol

--- CitiGalsV1/src/auto.c
+++ CitiGalsV1/src/auto.c
@@ -27,4 +27,19 @@
  * so, the robot will await a switch to another mode or disable/enable cycle.
  */
 void autonomous() {
+
+  chassisSet(127, 127); // drive forward
+  delay(3500); // delay 3.5 seconds = 3500 milliseconds
+  chassisSet(0, 0); // stop driving forward
+  liftSet(127); // lift goes up
+  delay(2000);
+  liftSet(0);
+  for(int i = 0; i < 3; i++) {
+   chassisSet(-127, 127); // rotate left
+   delay(1000);
+   chassisSet(127, -127); // rotate right for twice as long \\--/
+   delay(2000);
+  }
+  chassisSet(0, 0); // stop chassis for safety, although potentially not necessary
+
 }
